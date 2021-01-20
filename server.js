58fb9a02063608287c847803be234da183eb5c64
Samuel Fox <samueljasonfox@gmail.com>
2021-01-20 00:01:13 -0800

    added 'view data' functions to go along with the 'add data' functions

--- server.js
+++ server.js
@@ -14,44 +14,108 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
-    console.log('\n====== WELCOME!===========================================')
+    console.log('\n----------- WELCOME TO MySQL EMPLOYEE TRACKER! -----------')
     home();
 });
 
-
 function home() {
-    console.log('\n========================== HOME ==========================\n')
+    console.log('\n-------------------------- HOME --------------------------\n')
+    inquirer
+        .prompt([{
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['Add departments, roles, employees', 'View departments, roles, employees', 'QUIT'],
+            name: 'x'
+        }]).then(({ x }) => {
+            switch (x) {
+                case 'Add departments, roles, employees':
+                    return addData();
+                case 'View departments, roles, employees':
+                    return viewData();
+                default:
+                    console.log('\n------------------------ GOODBYE -------------------------\n')
+                    return connection.end();
+            }
+        })
+}
+
+function addData() {
+    console.log('\n------------------------ ADD DATA ------------------------\n')
     console.log(`!! IMPORTANT !!\n\nSTART by adding all departments\nTHEN add all employee role's\nEND with adding employees\n`)
     inquirer
         .prompt([{
             type: "list",
-            message: "What would you like to do?",
-            choices: ['Add Department', 'Add Role', 'Add Employee', 'QUIT'],
+            message: "What would you like to add?",
+            choices: ['Department', 'Role', 'Employee', '<-- exit'],
             name: 'choice'
         }]).then(({ choice }) => {
             switch (choice) {
-                case 'Add Department':
+                case 'Department':
                     return addDepartment();
-                case 'Add Role':
+                case 'Role':
                     return addRole();
-                case 'Add Employee':
+                case 'Employee':
                     return addEmployee();
                 default:
-                    console.log('\n======================== GOODBYE =========================\n')
-                    return connection.end();
+                    return home();
             }
         })
 
 }
 
-function addEmployee() {
-    console.log('\n======================== EMPLOYEE ========================')
-    home()
+function viewData() {
+    console.log('\n----------------------- VIEW DATA ------------------------\n')
+    inquirer
+        .prompt([{
+            type: 'list',
+            message: 'What would you like to view?',
+            choices: ['Departments', 'Roles', 'Employees', '<-- exit'],
+            name: 'x'
+        }]).then(({ x }) => {
+            switch (x) {
+                case 'Departments':
+                    return viewDepartments();
+                case 'Roles':
+                    return viewRoles();
+                case 'Employees':
+                    return viewEmployees();
+                default:
+                    return home();
+            }
+        })
+}
+
+
+function addDepartment() {
+    console.log('\n--------------------- ADD DEPARTMENT ---------------------')
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter a department name: ',
+                name: 'departmentName'
+            }
+        ]).then(({ departmentName }) => {
+            connection.query(
+                'INSERT INTO department SET ?',
+                {
+                    name: departmentName
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log('\nDepartment created successfully!')
+                    home()
+                });
+        })
+}
+function viewDepartments() {
+    console.log('\n-------------------- VIEW DEPARTMENTS --------------------')
+    viewData()
 }
 
 
 function addRole() {
-    console.log('\n========================== ROLE ==========================')
+    console.log('\n--------------------- ADD ROLE ------------------------')
     connection.query("SELECT * FROM department", (err, results) => {
         if (err) throw err;
 
@@ -99,27 +163,17 @@ function addRole() {
             })
     });
 }
+function viewRoles() {
+    console.log('\n----------------------- VIEW ROLES -----------------------')
+    viewData()
+}
 
 
-function addDepartment() {
-    console.log('\n======================= DEPARTMENT =======================')
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'Enter a department name: ',
-                name: 'departmentName'
-            }
-        ]).then(({ departmentName }) => {
-            connection.query(
-                'INSERT INTO department SET ?',
-                {
-                    name: departmentName
-                },
-                function (err) {
-                    if (err) throw err;
-                    console.log('\nDepartment created successfully!')
-                    home()
-                });
-        })
+function addEmployee() {
+    console.log('\n---------------------- ADD EMPLOYEE ----------------------')
+    home()
+}
+function viewEmployees() {
+    console.log('\n--------------------- VIEW EMPLOYEES ---------------------')
+    viewData()
 }
